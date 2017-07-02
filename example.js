54ea32145a7cadc88da45518eace9431d9e31196
Nick Thomas <nichoth@gmail.com>
2017-07-01 22:36:32 -0700

    first commit

---
+++ example.js
@@ -0,0 +1,43 @@
+var compose = require('compose-function')
+var c = require('./')
+
+var excitedly = compose(
+    c.join,
+    c.map(function (data) {
+        return c.either(
+            function onErr (err) {
+                return c.of('booo')
+            },
+            function (val) {
+                return c.of(val)
+            },
+            someIO(data + ' wooo')
+        )
+    }),
+    c.map(function (data) {
+        return data + '!!!'
+    })
+)
+
+excitedly(someIO('this is a value'))(function (err, val) {
+    console.log(err, val)
+})
+
+function someIO (data, cb) {
+    if (!cb) return function (_cb) {
+        return someIO(data, _cb)
+    }
+    process.nextTick(function () {
+        cb(null, data)
+    })
+}
+
+function ioError (data, cb) {
+    if (!cb) return function (_cb) {
+        return ioError(data, _cb)
+    }
+    process.nextTick(function () {
+        cb(new Error('rarrr'))
+    })
+}
+
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+    // take a nested continuable and return the inner one
+    join: function join (con) {
+        return function (cb) {
+            con(function (err, _con) {
+                if (err) return cb(err)
+                _con(cb)
+            })
+        }
+    },
+
+    map: function map (fn, con) {
+        if (!con) return function (c) {
+            return map(fn, c)
+        }
+        return function (cb) {
+            con(function (err, val) {
+                if (err) return cb(err)
+                cb(null, fn(val))
+            })
+        }
+    },
+
+    either: function either (left, right, con) {
+        if (!con) return function (_con) {
+            return either(left, right, _con)
+        }
+
+        return function (cb) {
+            con(function (err, val) {
+                if (err) return left(err)(cb)
+                return right(val)(cb)
+            })
+        }
+    },
+
+    of: function (val) {
+        return function (cb) {
+            cb(null, val)
+        }
+    }
+}
+
