9de1a0dcffc7b1e4db30aba85d3e868105befcc9
Nick Thomas <nichoth@gmail.com>
2017-09-21 11:39:33 -0700

    del comments

--- index.d.ts
+++ index.d.ts
@@ -15,7 +15,9 @@ export function map<A, B> (
 
 export function of<A> (value: A): Continuable<A>
 
-export function join<A> (continuable: Continuable<Continuable<A>>): Continuable<A>
+export function join<A> (
+    continuable: Continuable<Continuable<A>>
+): Continuable<A>
 
 export function either<A, B> (
     left: (err: Error) => Continuable<B>,
@@ -24,27 +26,3 @@ export function either<A, B> (
 ): Continuable<B>
 
 
-
-/*~ You can declare types that are available via importing the module */
-// export interface someType {
-//     name: string;
-//     length: number;
-//     extras?: string[];
-// }
-
-/*~ You can declare properties of the module using const, let, or var */
-// export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-// export namespace subProp {
-//     /*~ For example, given this definition, someone could write:
-//      *~   import { subProp } from 'yourModule';
-//      *~   subProp.foo();
-//      *~ or
-//      *~   import * as yourMod from 'yourModule';
-//      *~   yourMod.subProp.foo();
-//      */
-//     export function foo(): void;
-// }
